60c3b4baa314f4d6804f7664023b71d1f246964a
Vladimir Oleinikov <dragon2020@inbox.ru>
2021-08-03 20:41:26 +0300

    feat: create navigazione controller

---
+++ Drawing pictures/Controllers/NavigationController.h
@@ -0,0 +1,17 @@
+//
+//  NavigationController.h
+//  Drawing pictures
+//
+//  Created by Vladimir Oleinikov on 03.08.2021.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIFont+MyFont.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NavigationController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
